7eecf9c266dfea62c0c2e3948e505cad5c31ce5e
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-08-10 16:22:06 +0500

    fix html file

--- server.mjs
+++ server.mjs
@@ -11,7 +11,7 @@ app.use(express.json());
 //     res.send("Hello ToDo App");
 // });
 app.get('/', express.static('public'));
-app.use('/', express.static('public'));
+// app.use('/', express.static('public'));
 app.use('/static', express.static('static'));
 
 app.use((req, res) => {
