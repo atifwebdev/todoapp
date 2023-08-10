e454fc2855c99311614ce54cb9330ce6cc8eab8d
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-08-10 16:19:08 +0500

    connect html file

--- server.mjs
+++ server.mjs
@@ -7,16 +7,16 @@ app.use(express.json());
 
 
 // Root url
-app.get( "/", (req, res) => {
-    res.send("Hello ToDo App");
-});
-// app.get('/', express.static('public'));
-// app.use('/', express.static('public'));
-// app.use('/static', express.static('static'));
-
-// app.use((req, res) => {
-//     res.status(404).send("not found");
+// app.get( "/", (req, res) => {
+//     res.send("Hello ToDo App");
 // });
+app.get('/', express.static('public'));
+app.use('/', express.static('public'));
+app.use('/static', express.static('static'));
+
+app.use((req, res) => {
+    res.status(404).send("not found");
+});
 
 
 const port = process.env.PORT || 3000;
