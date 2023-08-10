26350019a6f703f6f42ee66ebd524cb01635ec6a
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-08-10 16:37:37 +0500

    fix issue

--- server.mjs
+++ server.mjs
@@ -1,5 +1,6 @@
 import express from "express";
-
+import path from 'path';
+const __dirname = path.resolve();
 
 
 const app = express();
@@ -10,9 +11,13 @@ app.use(express.json());
 // app.get( "/", (req, res) => {
 //     res.send("Hello ToDo App");
 // });
-app.get('/', express.static('public'));
-// app.use('/', express.static('public'));
-app.use('/static', express.static('static'));
+
+// app.get('/', express.static('public'));
+// app.use('/static', express.static('static'));
+
+app.get(express.static(path.join(__dirname, "public")));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.use((req, res) => {
     res.status(404).send("not found");
